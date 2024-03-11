bf06c368713513cd41812fede958e7e86bb43ce6
Maymeric <aymeric.marec@gmail.com>
2024-03-11 16:28:16 +0100

    super nom de commit

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+var buttonColor = "green"
 
 func main() {
 	http.HandleFunc("/", homeHandler)
@@ -38,13 +39,14 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		log.Println("Message reçu:", message, "type de message : ", messageType)
-
+		buttonColor = string(message)
 		// Vous pouvez ajouter ici la logique pour diffuser le message à tous les autres clients
 		// ou effectuer toute autre action en fonction du message reçu
 		// Par exemple, si le message est une couleur, vous pouvez le diffuser à tous les autres clients connectés.
 
 		// Vous pouvez également envoyer une réponse au client si nécessaire.
 		// Par exemple, si le client envoie une demande de confirmation, vous pouvez répondre avec une confirmation.
+		err = conn.WriteMessage(messageType, []byte(buttonColor))
 		if err := conn.WriteMessage(messageType, message); err != nil {
 			log.Println(err)
 			return
