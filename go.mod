9dfc05953e9b7a0362c53b7cae0aa53d02bc9caf
Diane SAUTEREAU DU PART <diane.sautereau@ynov.com>
2024-03-11 16:02:45 +0100

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module infra
 
-go 1.21.0
+go 1.21
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,66 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"text/template"
+
+	"github.com/gorilla/websocket"
 )
+
+var clients = make(map[*websocket.Conn]bool) // Map pour stocker les connexions clients
+var broadcast = make(chan string)            // Channel de diffusion des messages
+
+var upgrader = websocket.Upgrader{} // Upgrader pour la mise à niveau de la connexion HTTP à une connexion WebSocket
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ws", wsHandler) // Nouvelle route pour gérer les connexions WebSocket
 
-	http.ListenAndServe("localhost:8080", nil)
+	go handleMessages() // Démarrer la goroutine pour gérer les messages diffusés aux clients
+
+	fmt.Println("Serveur démarré sur http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("./index.html")
+	http.ServeFile(w, r, "index.html")
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Mise à niveau de la connexion HTTP à une connexion WebSocket
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	defer ws.Close()
 
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// Ajouter la nouvelle connexion client à la map
+	clients[ws] = true
+
+	// Attendre les messages du client (ceci peut être omis si vous n'avez pas besoin de recevoir des messages du client)
+	for {
+		var msg string
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+	}
+}
+
+func handleMessages() {
+	for {
+		// Récupérer le message de la chaîne de diffusion
+		msg := <-broadcast
+
+		// Envoyer le message à tous les clients connectés
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				delete(clients, client)
+				client.Close()
+			}
+		}
 	}
 }
