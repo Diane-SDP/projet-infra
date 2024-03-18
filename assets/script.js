0ed600dc36213f0b1e70786957aa0b224dfb645d
Diane SAUTEREAU DU PART <diane.sautereau@ynov.com>
2024-03-18 16:47:35 +0100

    morpion

---
+++ assets/script.js
@@ -0,0 +1,116 @@
+var ws = new WebSocket("ws://82.67.64.82:80/ws");
+var color = document.getElementById("jscolor")
+var getcolor = document.getElementById("getcolor")
+var getcode = document.getElementById("getcode")
+var urlcourante = document.location.href;
+var currentCode = urlcourante.split("/")[4]
+var buttoncouleur = document.getElementById('colorButton');
+var bouton1 = document.getElementById("1")
+var bouton2 = document.getElementById("2")
+var bouton3 = document.getElementById("3")
+var bouton4 = document.getElementById("4")
+var bouton5 = document.getElementById("5")
+var bouton6 = document.getElementById("6")
+var bouton7 = document.getElementById("7")
+var bouton8 = document.getElementById("8")
+var bouton9 = document.getElementById("9")
+var statut = document.getElementById("statut")
+let buttons = [[bouton1, bouton2, bouton3], [bouton4, bouton5, bouton6], [bouton7, bouton8,bouton9]]
+var tour = true
+var droitdejouer = true
+
+ws.onopen = function(event) {
+    console.log("WebSocket connected.");
+};
+
+ws.onerror = function(error) {
+    console.error('WebSocket Error:', error);
+};
+        
+function Win(symb) {
+    for(var i = 0 ; i < 3 ; i++){
+        if (buttons[i][0] == symb && buttons[i][1] == symb && buttons[i][2] == symb) {
+            return true;
+        }
+    }
+	for (var col = 0; col < 3;col++) {
+		if (buttons[0][col] == symb && buttons[1][col] == symb && buttons[2][col] == symb) {
+			return true;
+		}
+	}
+	if (buttons[0][0] == symb && buttons[1][1] == symb && buttons[2][2] == symb) {
+		return true;
+	}
+	if (buttons[0][2] == symb && buttons[1][1] == symb && buttons[2][0] == symb) {
+		return true;
+	}
+}
+     
+
+function Egalite(){
+    Egalite = true
+    for(var i = 0 ; i < 9 ; i++){
+        if(document.getElementById(i).innerText == ""){
+            Egalite = false
+        }
+    }
+    if (Egalite){
+        statut.textContent = "Egalité sale merde"  
+    }   
+}
+
+function Morpion(nEvent){
+    currentbtn = document.getElementById(nEvent.id)
+    if (currentbtn.innerText == ""){
+        if (tour && droitdejouer){
+            droitdejouer = false
+            currentbtn.innerText = "X"
+            ws.send(getcode.value+"|"+false);
+            if (Win("X")) {
+                statut.textContent = "Le joueur X a gagné"
+            } else {
+                statut.textContent = "tour du j1"
+            }
+            
+        }else if (droitdejouer){
+            droitdejouer = false
+            currentbtn.innerText = "0"
+            ws.send(getcode.value+"|"+true);
+            if (Win("O")) {
+                statut.textContent = "Le joueur O a gagné"
+            } else {
+                statut.textContent = "tour du j2"
+            }
+        }
+    }
+    Egalite()
+       
+}
+
+
+
+
+function changeColor(nEvent) {
+    var btntemp = document.getElementById(nEvent.id)
+    var newColor = btntemp.style.backgroundColor === 'green' ? 'red' : 'green';
+    console.log("tient :",newColor)
+    ws.send(newColor + "|" + getcode.value+"|"+tour);
+    console.log("hidden input",getcolor.value)
+    console.log("pute",getcode.value)
+    btntemp.style.backgroundColor = newColor;
+}
+ws.onmessage = function(event) {
+    var button = document.getElementById('colorButton');
+    var coderecu = event.data.split('|')[0].substring(1);
+    var tourrecu = event.data.split('|')[1]
+    console.log("currentcode", currentCode)
+    console.log("code recu", coderecu)
+    if (coderecu == currentCode) {
+        console.log("recup :",event.data)
+        tour = tourrecu
+        droitdejouer = true
+    }  
+};
+ws.onclose = function(event) {
+    console.log("websocket closed")
+}
