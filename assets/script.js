ea46de2b11dc74161ef470864fe66570e3a5c907
Diane SAUTEREAU DU PART <diane.sautereau@ynov.com>
2024-03-19 14:05:31 +0100

    fix droitdejouer

--- assets/script.js
+++ assets/script.js
@@ -67,7 +67,7 @@ function Morpion(nEvent){
         if (tour && droitdejouer){
             droitdejouer = false
             currentbtn.innerText = "X"
-            ws.send(getcode.value+"|"+false+"|",nEvent.id);
+            ws.send(getcode.value+"|"+"false"+"|",nEvent.id);
             if (Win("X")) {
                 statut.textContent = "Le joueur X a gagné"
             } else {
@@ -77,7 +77,7 @@ function Morpion(nEvent){
         } else if (droitdejouer){
             droitdejouer = false
             currentbtn.innerText = "0"
-            ws.send(getcode.value+"|"+true+"|",nEvent.id);
+            ws.send(getcode.value+"|"+"true"+"|",nEvent.id);
             if (Win("O")) {
                 statut.textContent = "Le joueur O a gagné"
             } else {
@@ -105,11 +105,13 @@ ws.onmessage = function(event) {
     var button = document.getElementById('colorButton');
     var coderecu = event.data.split('|')[0].substring(1);
     var tourrecu = event.data.split('|')[1]
-    console.log("currentcode", currentCode)
-    console.log("code recu", coderecu)
-    if (coderecu == currentCode && !droitdejouer) {
+    if (coderecu == currentCode) {
         console.log("recup :",event.data)
-        tour = tourrecu
+        if (tourrecu == "false") {
+            tour = false
+        } else {
+            tour = true
+        }
         droitdejouer = true
     }  
 };
